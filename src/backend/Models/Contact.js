bfd80a56b2f5d6e2b18fdbd54311205ee231716c
flora200198 <aflora398@gmail.com>
2025-08-26 12:16:55 +0530

    Contact Schema updated

---
+++ src/backend/Models/Contact.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema(
+    {
+        name : { type: String, required: true,},
+        phone : { type: String, required: true},
+        message : { type: String}
+    },
+    { timestamps: true }
+)
+
+const Contact = mongoose.model( 'Contact', ContactSchema, 'contact');
+module.exports = Contact;
